959aabdca532e740fd045deb45e748451bc69d63
nandini@26 <84126052+Nandini-Mahurkar@users.noreply.github.com>
2023-11-02 01:55:19 +0530

    changes made

--- src/app.test.js
+++ src/app.test.js
@@ -2,6 +2,6 @@ const { dayOfTheWeek } = require('./app');
 
 
 test('getDay returns the long-format day of the week', () => {
-    const day = dayOfTheWeek( new Date('1/11/2023') );
-    expect( day ).toBe('Wednesday');
+    const day = dayOfTheWeek( new Date('2/11/2023') );
+    expect( day ).toBe('Thursday');
 });
